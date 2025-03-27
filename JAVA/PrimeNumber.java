5fa83c772da706451bf581fb3c2eb321fae4f5a8
saisj2002 <sairajjawake2002@gmail.com>
2025-03-27 09:47:02 +0530

    lecture commit

---
+++ JAVA/PrimeNumber.java
@@ -0,0 +1,34 @@
+// import java.util.Scanner;
+
+// public class PrimeNumber {
+//     public static void main(String[] args) {
+//         // Scanner sc = new Scanner(System.in);
+//         // System.out.println("Enter a Number: ");
+//         // int n = sc.nextInt();
+
+//         // if(n%2!=0 || n==2){
+//         //     System.out.println("Is a Prime Number");
+//         // }else{
+//         //     System.out.println("It Not a Prime Number");
+//         // }
+
+//         // sc.close();
+
+//         // Scanner sc = new Scanner(System.in);
+//         // System.out.println("Enter the Range of Numbers: ");
+//         // int a = sc.nextInt();
+//         // int b = sc.nextInt();
+
+        
+//         // int sum = 0;
+//         // for(int i=a;a<=b;a++){
+//         //     if(a%2!=0 || a==2){
+//         //         sum+=a;
+//         //     }
+//         //     }
+
+//         // System.out.println(sum);
+
+//         // sc.close();
+//     }
+// }
---
+++ JAVA/stars.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class stars {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of stars: ");
+        int n = sc.nextInt();
+
+            for(int i=0;i<n;i++){
+                System.out.print("*");
+            }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(2*i)+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(2*(n-i))-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        
+        int a=n/2;
+        int b=1;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<a;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<b;j++){
+                System.out.print("*");
+            }
+           
+
+            if(i<n/2){
+                b+=2;
+                a-=1;
+            }else{
+                b-=2;
+                a+=1;
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }   
+
+        System.out.println();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(2*i)+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+
+        System.out.println();
+
+        
+        
+        sc.close();
+    }
+}
