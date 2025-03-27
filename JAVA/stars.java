c7a39e5a444c95ebecc7bccb9571becdc8cbaf37
saisj2002 <sairajjawake2002@gmail.com>
2025-03-27 11:07:54 +0530

    lecture commit

--- JAVA/stars.java
+++ JAVA/stars.java
@@ -20,29 +20,6 @@ public class stars {
 
         System.out.println();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-i;j++){
-                System.out.print(" ");
-            }
-            for(int j=0;j<(2*i)+1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print(" ");
-            }
-            for(int j=0;j<(2*(n-i))-1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        System.out.println();
-
-        
         int a=n/2;
         int b=1;
 
@@ -98,8 +75,48 @@ public class stars {
 
         System.out.println();
 
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=0;j<(2*i)+1;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i=1;i<n;i++){
+        //     for(int j=0;j<=i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=0;j<(2*(n-i))-1;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
         
-        
+        int c=n-1;
+        int d=1;
+
+        for(int i=0;i<n+(n-1);i++){
+            for(int j=0;j<c;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<d;j++){
+                System.out.print("*");
+            }
+           
+
+            if(i<n-1){
+                d+=2;
+                c-=1;
+            }else{
+                d-=2;
+                c+=1;
+            }
+
+            System.out.println();
+        }
+
         sc.close();
     }
 }
---
+++ read.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read -p "what is your name? " name
+echo "Hii" $name," How are you?"
+bash push.sh
