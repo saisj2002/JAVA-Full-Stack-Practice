510223ce4167f65118bbe7bac84c7d1ba793007c
saisj2002 <sairajjawake2002@gmail.com>
2025-04-23 12:27:20 +0530

    lecture commit

--- Web Technologies/JS/commonMethods.js
+++ Web Technologies/JS/commonMethods.js
@@ -2,6 +2,8 @@ let firstName = "Sai";
 let middleName = " Pramod";
 let lastName = " Jawake.";
 
+let data = [2,4,1,5,3,7,6,9,8,0];
+
 let fullName = firstName.concat(middleName, lastName);
 let fullNameArray = fullName.split("");
 
@@ -14,3 +16,5 @@ console.log("             Sai Pramod Jawake.           ".trim());
 console.log(fullNameArray);
 console.log(fullNameArray.sort((a, b) => a.localeCompare(b)));
 console.log(fullNameArray.sort((a, b) => b.localeCompare(a)));
+console.log(data.sort((a, b) => a-b));
+console.log(data.sort((a, b) => b-a));
